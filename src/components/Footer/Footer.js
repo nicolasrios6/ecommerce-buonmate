142445a9998f9971027d89820243848b1f80a080
Nicolás Ríos <riosnicolas618@gmail.com>
2022-09-08 13:02:22 -0300

    Link a redes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -15,9 +15,9 @@ const Footer = () => {
                 <p className='footer__container__info'>Azcuénaga 1451, Guaymallén, Mendoza.</p>
             </div>
             <ul className='footer__socialMedia'>
-                <li><a href='https://api.whatsapp.com/send/?phone=5492613031702&text&type=phone_number&app_absent=0'><WhatsAppIcon className='socialMedia__icons' /></a></li>
-                <li><a href='https://www.facebook.com/buon.mate.7'><FacebookIcon className='socialMedia__icons' /></a></li>
-                <li><a href='https://www.instagram.com/buon.mate/'><InstagramIcon className='socialMedia__icons' /></a></li>
+                <li><a href='https://api.whatsapp.com/send/?phone=5492613031702&text&type=phone_number&app_absent=0' target='_blank'><WhatsAppIcon className='socialMedia__icons' /></a></li>
+                <li><a href='https://www.facebook.com/buon.mate.7' target='_blank'><FacebookIcon className='socialMedia__icons' /></a></li>
+                <li><a href='https://www.instagram.com/buon.mate/' target='_blank'><InstagramIcon className='socialMedia__icons' /></a></li>
             </ul>
             <p className='footer__copy'>© Buon Mate</p>
         </div>
